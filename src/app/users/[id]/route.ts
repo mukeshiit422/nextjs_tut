16ad3e1e0837494ab44b47cb2d7e184cec805021
mukesh-compiify <mukesh@compiify.com>
2025-03-06 19:32:48 +0530

    fix

--- src/app/users/[id]/route.ts
+++ src/app/users/[id]/route.ts
@@ -1,7 +1,7 @@
 import { users } from "../route";
 
 export async function GET (_request: Request, 
-    {params}:{params :{id : string}}
+    { params }:{ params :{id : string}}
    ) {
     const {id} = await params ;
     const user = users.find(user => user.id === parseInt(id));
